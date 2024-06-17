263477d0baeffec84c1d32064d9a6c80bb760eba
Fialho <Guillermoezequielfg@gmail.com>
2024-06-17 15:29:14 -0300

    creacion del la estructura del proyecto

---
+++ Programacion/Proyecto/Api-Post-Eventos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_Post_Eventos
+{
+    public class Class1
+    {
+    }
+}
